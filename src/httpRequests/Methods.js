e48ba4e6566f36424eceb9a4e9c07615babaee24
joezam1 <josezamorano@protonmail.com>
2021-08-01 19:13:41 +1000

    updated cors method

--- src/httpRequests/Methods.js
+++ src/httpRequests/Methods.js
@@ -13,6 +13,9 @@ var getMethod = function(url, responseCallback){
     
     const options={
         method:'GET',
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
         headers: authHeaders
     }
         
